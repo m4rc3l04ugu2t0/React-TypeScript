cb137f48511197b4655c4d082339a8e2204d7528
Marcelo Augusto <mrcelo014@gmail.com>
2024-02-09 22:01:37 -0300

    Typing Props

---
+++ react-typescript/src/app/page.tsx
@@ -0,0 +1,10 @@
+import HelloName from "@/components/HelloName/page";
+
+export default function Home() {
+    return (
+        <main>
+            <h1 className="text-3xl font-bold underline">React + Typescript</h1>
+            <HelloName name="Marcelo" />
+        </main>
+    );
+}
